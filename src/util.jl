7a947ae7545b2d2ad0ff50750812c1ee5f7bad6f
Guillaume Lam <guillaume.lam10100@gmail.com>
2021-11-01 18:23:43 -0400

    Split util file into smaller files

--- src/util.jl
+++ src/util.jl
@@ -1,38 +1,3 @@
-struct LSM_Params{I<:Real, F<:Real}
-    n_in::I
-    res_in::I
-
-    ne::I
-    ni::I
-
-    res_out::I
-    n_out::I
-
-    K::I
-    C::I
-
-    PE_UB::F
-    EE_UB::F
-    EI_UB::F
-    IE_UB::F
-    II_UB::F
-
-    LSM_Params(
-        n_in::I,res_in::I,ne::I,ni::I,res_out::I,n_out::I,K::I,C::I,
-        PE_UB::F,EE_UB::F,EI_UB::F,IE_UB::F,II_UB::F
-        ) where {I<:Real,F<:Real} = new{I,F}(n_in,res_in,ne,ni,res_out,n_out,K,C,PE_UB,EE_UB,EI_UB,IE_UB,II_UB)
-
-    LSM_Params(
-            n_in::I,res_in::I,ne::I,ni::I,res_out::I,n_out::I,K::I,C::I
-            ) where {I<:Number} = LSM_Params(n_in,res_in,ne,ni,res_out,n_out,K,C,0.6,0.005,0.25,0.3,0.01)
-
-    LSM_Params(n_in::I, n_out::I, env::String) where {I<:Real} = (
-        if env == "cartpole"
-            return LSM_Params(n_in,32,120,30,32,n_out,3,4)
-        end
-    )
-end
-
 function create_conn(val, avg_conn, ub, n)
     if val < avg_conn/n
         return rand()*ub
