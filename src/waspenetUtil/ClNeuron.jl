5036e6740890440e35702b289bf5789ae38ca0c2
Guillaume Lam <guillaume.lam10100@gmail.com>
2021-11-01 18:25:08 -0400

    Renamed folder

--- src/waspenetUtil/ClNeuron.jl
+++ src/waspenetUtil/ClNeuron.jl
@@ -1,6 +1,8 @@
 struct ClNeuron{N<:AbstractNeuron, T<:Number} <: AbstractNeuron
     inner_neuron::N
 
+    t_sls::T = 0    # Time since last spike
+
     c::T = 5.
     cθ::T = 5.
     cδ::T = 3.
@@ -10,6 +12,12 @@ function update(neuron::ClNeuron, input_update, dt, t)
     inner_output, return_neuron = update(neuron.inner_neuron, input_update, dt, t)
 
     # Update c based on calcium dynamics
+
+    if inner_output == 1
+        neuron.t_sls = 0
+    else
+        neuron.t_sls += 1
+    end
 
     return (inner_output, ClNeuron(return_neuron))
 end
